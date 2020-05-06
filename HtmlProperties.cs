2ce7f73f20c2ecb06c05c50c917d39146c9c777b
snasphysicist <ih8coldplay@hotmail.com>
2020-05-06 21:47:34 +0800

    Constructor for HtmlProperties

--- HtmlProperties.cs
+++ HtmlProperties.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic.LinkedList;
 
 namespace MarkdownToHtml
 {
-    public class MarkdownToHtml
+    public class HtmlProperties
     {
 
-        LinkedList requiredAttributes;
+        LinkedList<String> requiredAttributes;
 
         string Tag
         { get; set; }
+
+        HtmlProperties (
+            string tag
+        ) {
+            Tag = tag;
+            requiredAttributes = new LinkedList<String>();
+        }
 
     }
 }
