45247aa09b7a2fd16529ab6feba61ff53960e2e4
snasphysicist <ih8coldplay@hotmail.com>
2020-05-06 21:27:01 +0800

    Enum for markdown element types

---
+++ MarkdownElementTypes.cs
@@ -0,0 +1,26 @@
+
+namespace MarkdownToHtml
+{
+    enum MarkdownElementTypes
+    {
+        Heading1,
+        Heading2,
+        Heading3,
+        Heading4,
+        Heading5,
+        Heading6,
+        Heading1Underlined,
+        Heading2Underlined,
+        Emphasis,
+        Strong,
+        Strikethrough,
+        OrderedList,
+        UnorderedList,
+        Link,
+        Image,
+        CodeInline,
+        CodeBlock,
+        Quote,
+        HorizontalRule
+    }
+}
