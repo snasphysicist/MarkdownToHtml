4b18dd4dcb7bcc77faee52ec978b8a50aee4aa21
snasphysicist <ih8coldplay@hotmail.com>
2020-05-09 17:51:11 +0800

    Separate test classes for different element types that can occur nested within text

--- MarkdownToHtml.Tests/MarkdownEmphasisTests.cs
+++ MarkdownToHtml.Tests/MarkdownEmphasisTests.cs
@@ -4,38 +4,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace MarkdownToHtml 
 {
     [TestClass]
-    public class MarkdownParagraphTests
+    public class MarkdownEmphasisTests
     {
-        [DataTestMethod]
-        [DataRow("Test1")]
-        public void ShouldParseCorrectlyFormattedPlainTextLineSuccess(
-            string markdown
-        ) {
-            MarkdownParser parser = new MarkdownParser(
-                new string[] {
-                    markdown
-                }
-            );
-            Assert.IsTrue(
-                parser.Success
-            );
-            // Should produce exactly one piece of content
-            Assert.IsTrue(
-                parser.Content.Length == 1
-            );
-            string html = parser.Content[0].ToHtml();
-            // HTML should contain the provided text
-            Assert.IsTrue(
-                html.Contains(markdown)
-            );
-            // HTML should contain open/close paragraph tags
-            Assert.IsTrue(
-                html.Contains("<p>")
-            );
-            Assert.IsTrue(
-                html.Contains("</p>")
-            );
-        }
 
         [DataTestMethod]
         [DataRow("*test1*", "<p><emph>test1</emph></p>")]
---
+++ MarkdownToHtml.Tests/MarkdownPlainTextTests.cs
@@ -0,0 +1,41 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarkdownToHtml 
+{
+    [TestClass]
+    public class MarkdownPlainTextTests
+    {
+        [DataTestMethod]
+        [DataRow("Test1")]
+        public void ShouldParseCorrectlyFormattedPlainTextLineSuccess(
+            string markdown
+        ) {
+            MarkdownParser parser = new MarkdownParser(
+                new string[] {
+                    markdown
+                }
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            // Should produce exactly one piece of content
+            Assert.IsTrue(
+                parser.Content.Length == 1
+            );
+            string html = parser.Content[0].ToHtml();
+            // HTML should contain the provided text
+            Assert.IsTrue(
+                html.Contains(markdown)
+            );
+            // HTML should contain open/close paragraph tags
+            Assert.IsTrue(
+                html.Contains("<p>")
+            );
+            Assert.IsTrue(
+                html.Contains("</p>")
+            );
+        }
+        
+    }
+}
