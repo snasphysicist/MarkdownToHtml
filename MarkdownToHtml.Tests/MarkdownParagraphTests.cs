95d12f327622e7ecdac0775301ecbb03f0af7c5c
snasphysicist <ih8coldplay@hotmail.com>
2020-05-10 13:45:44 +0800

    Tests for paragraph line group parsing, all failing

---
+++ MarkdownToHtml.Tests/MarkdownParagraphTests.cs
@@ -0,0 +1,71 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarkdownToHtml 
+{
+    [TestClass]
+    public class MarkdownParagraphTests
+    {
+        [TestMethod]
+        public void ShouldParseTwoAdjacentLinesAsOneParagraphSuccess() {
+            string[] testData = new string[]
+            {
+                "test1",
+                "test2"
+            };
+            string targetHtml = "<p>test1 test2</p>";
+            MarkdownParser parser = new MarkdownParser(
+                testData
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            Assert.AreEqual(
+                targetHtml,
+                parser.ToHtml()
+            );
+        }
+
+        [TestMethod]
+        public void ShouldParseTwoSeparatedLinesAsTwoParagraphsSuccess() {
+            string[] testData = new string[]
+            {
+                "test1",
+                " ",
+                "test2"
+            };
+            string targetHtml = "<p>test1</p><p>test2</p>";
+            MarkdownParser parser = new MarkdownParser(
+                testData
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            Assert.AreEqual(
+                targetHtml,
+                parser.ToHtml()
+            );
+        }
+
+        [TestMethod]
+        public void ShouldParseTwoAdjacentLinesWithTwoSpacesAsOneParagraphWithLineBreakSuccess() {
+            string[] testData = new string[]
+            {
+                "test1  ",
+                "test2"
+            };
+            string targetHtml = "<p>test1<br/>test2</p>";
+            MarkdownParser parser = new MarkdownParser(
+                testData
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            Assert.AreEqual(
+                targetHtml,
+                parser.ToHtml()
+            );
+        }
+        
+    }
+}
