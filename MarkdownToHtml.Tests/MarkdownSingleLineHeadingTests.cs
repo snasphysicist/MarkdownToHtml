add563cd45cacbe7073b81e4fc4c8d7d36a8da19
snasphysicist <ih8coldplay@hotmail.com>
2020-05-10 22:43:23 +0800

    Updated single line heading tests based on update knowledge about the specifications

--- MarkdownToHtml.Tests/MarkdownSingleLineHeadingTests.cs
+++ MarkdownToHtml.Tests/MarkdownSingleLineHeadingTests.cs
@@ -8,13 +8,13 @@ namespace MarkdownToHtml
     {
 
         [DataTestMethod]
-        [DataRow("# test", "<h1>test</h1>")]
-        [DataRow("## test", "<h2>test</h2>")]
-        [DataRow("### test", "<h3>test</h3>")]
-        [DataRow("#### test", "<h4>test</h4>")]
-        [DataRow("##### test", "<h5>test</h5>")]
-        [DataRow("###### test", "<h6>test</h6>")]
-        public void ShouldParseCorrectlyFormattedSingleLineHeadingSuccess(
+        [DataRow("#test", "<h1>test</h1>")]
+        [DataRow("##test", "<h2>test</h2>")]
+        [DataRow("###test", "<h3>test</h3>")]
+        [DataRow("####test", "<h4>test</h4>")]
+        [DataRow("#####test", "<h5>test</h5>")]
+        [DataRow("######test", "<h6>test</h6>")]
+        public void ShouldParseCorrectlyFormattedNoSpaceSingleLineHeadingSuccess(
             string markdown,
             string targetHtml
         ) {
@@ -34,9 +34,72 @@ namespace MarkdownToHtml
         }
 
         [DataTestMethod]
-        [DataRow("#test", "<p>#test</p>")]
+        [DataRow("#   test", "<h1>test</h1>")]
+        public void ShouldParseCorrectlyFormattedWithSpaceSingleLineHeadingSuccess(
+            string markdown,
+            string targetHtml
+        ) {
+            MarkdownParser parser = new MarkdownParser(
+                new string[] {
+                    markdown
+                }
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            string html = parser.Content[0].ToHtml();
+            Assert.AreEqual(
+                targetHtml,
+                html
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("#test#", "<h1>test</h1>")]
+        [DataRow("#test###########", "<h1>test</h1>")]
+        public void ShouldParseCorrectlyFormattedWithTrailingHashesSingleLineHeadingSuccess(
+            string markdown,
+            string targetHtml
+        ) {
+            MarkdownParser parser = new MarkdownParser(
+                new string[] {
+                    markdown
+                }
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            string html = parser.Content[0].ToHtml();
+            Assert.AreEqual(
+                targetHtml,
+                html
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("#######test", "<h1>#test</h1>")]
+        [DataRow("##########test", "<h1>####test</h1>")]
+        public void ShouldParseTooManyLeadingHashesSingleLineHeadingSuccess(
+            string markdown,
+            string targetHtml
+        ) {
+            MarkdownParser parser = new MarkdownParser(
+                new string[] {
+                    markdown
+                }
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            string html = parser.Content[0].ToHtml();
+            Assert.AreEqual(
+                targetHtml,
+                html
+            );
+        }
+
+        [DataTestMethod]
         [DataRow(" # test", "<p> # test</p>")]
-        [DataRow("####### test", "<p>####### test</p>")]
         public void ShouldParseIncorrectlyFormattedSingleLineHeadingAsTextSuccess(
             string markdown,
             string targetHtml
