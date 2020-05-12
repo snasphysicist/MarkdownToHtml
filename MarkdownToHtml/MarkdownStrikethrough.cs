4d3639b6f24226087f0999a3e28a5b478b89595e
snasphysicist <ih8coldplay@hotmail.com>
2020-05-12 22:50:58 +0800

    Move functionality to parse a strikethrough section to the MarkdownStrikethrough class

--- MarkdownToHtml/MarkdownStrikethrough.cs
+++ MarkdownToHtml/MarkdownStrikethrough.cs
@@ -1,9 +1,14 @@
 
+using System.Text.RegularExpressions;
 
 namespace MarkdownToHtml
 {
     public class MarkdownStrikethrough : IHtmlable
     {
+
+        private static Regex regexParseable = new Regex(
+            @"^~{2}.*~{2}.*"
+        );
 
         IHtmlable[] content;
 
@@ -26,6 +31,51 @@ namespace MarkdownToHtml
             }
             html += $"</{tag}>";
             return html;
+        }
+
+        static bool CanParseFrom(
+            string line
+        ) {
+            return regexParseable.Match(line).Success;
+        }
+
+        static ParseResult ParseStrikethroughSection(
+            string line
+        ) {
+            ParseResult result = new ParseResult();
+            if (!CanParseFrom(line))
+            {
+                // Fail immediately if we cannot parse this text as strikethrough
+                result.Line = line;
+                return result;
+            }
+            int j = 2;
+            // Find closing tildes
+            while (
+                (j < line.Length)
+                && !(
+                    (line.Substring(j-1, 2) == "~~")
+                    && (line[j-2] != '\\')
+                )
+            ) {
+                j++;
+            }
+            if (j >= line.Length)
+            {
+                // Fail if we cannot find the closing squiggles
+                result.Line = line;
+                return result;
+            }
+            // Parse everything inside the stars
+            MarkdownStrikethrough element = new MarkdownStrikethrough(
+                MarkdownParser.ParseInnerText(
+                    line.Substring(2, j - 3)
+                )
+            );
+            result.AddContent(element);
+            result.Line = line.Substring(j + 1);
+            result.Success = true;
+            return result;
         }
 
     }
