a0352de917f22f578a04e8e19ed13666d9189a3d
snasphysicist <ih8coldplay@hotmail.com>
2020-05-12 23:24:36 +0800

    Update emphasis element tests for correct html tag (emph -> em)

--- MarkdownToHtml.Tests/MarkdownEmphasisTests.cs
+++ MarkdownToHtml.Tests/MarkdownEmphasisTests.cs
@@ -8,8 +8,8 @@ namespace MarkdownToHtml
     {
 
         [DataTestMethod]
-        [DataRow("*test1*", "<p><emph>test1</emph></p>")]
-        [DataRow("test1*test2*test3", "<p>test1<emph>test2</emph>test3</p>")]
+        [DataRow("*test1*", "<p><em>test1</em></p>")]
+        [DataRow("test1*test2*test3", "<p>test1<em>test2</em>test3</p>")]
         public void ShouldParseCorrectlyFormattedStarEmphasisLineSuccess(
             string markdown,
             string targetHtml
@@ -31,8 +31,8 @@ namespace MarkdownToHtml
         }
 
         [DataTestMethod]
-        [DataRow("_test1_", "<p><emph>test1</emph></p>")]
-        [DataRow("test1_test2_test3", "<p>test1<emph>test2</emph>test3</p>")]
+        [DataRow("_test1_", "<p><em>test1</em></p>")]
+        [DataRow("test1_test2_test3", "<p>test1<em>test2</em>test3</p>")]
         public void ShouldParseCorrectlyFormattedUnderscoreEmphasisLineSuccess(
             string markdown,
             string targetHtml
@@ -54,8 +54,8 @@ namespace MarkdownToHtml
         }
 
         [DataTestMethod]
-        [DataRow("*te\\*st1*", "<p><emph>te*st1</emph></p>")]
-        [DataRow("_te\\_st1_", "<p><emph>te_st1</emph></p>")]
+        [DataRow("*te\\*st1*", "<p><em>te*st1</em></p>")]
+        [DataRow("_te\\_st1_", "<p><em>te_st1</em></p>")]
         public void ShouldParseCorrectlyEscapedEmphasisCharactersSuccess(
             string markdown,
             string targetHtml
