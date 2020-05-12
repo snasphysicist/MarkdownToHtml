449823d4b9abaec876a784f8840848792b4db387
snasphysicist <ih8coldplay@hotmail.com>
2020-05-12 22:24:39 +0800

    Ensure all members of the class have default values on construction

--- MarkdownToHtml/ParseResult.cs
+++ MarkdownToHtml/ParseResult.cs
@@ -11,12 +11,14 @@ namespace MarkdownToHtml
         public string Line
         { get; set; }
 
-        public string Success
+        public bool Success
         { get; set; }
 
         public ParseResult()
         {
             content = new LinkedList<IHtmlable>();
+            Line = "";
+            Success = false;
         }
 
         public void AddContent(
