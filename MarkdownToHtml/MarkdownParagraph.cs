1ca5f25959668295733604538f8d9adccfa6d6ca
snasphysicist <ih8coldplay@hotmail.com>
2020-05-13 21:47:09 +0800

    Move paragraph parsing functionality to MarkdownParagraph class

--- MarkdownToHtml/MarkdownParagraph.cs
+++ MarkdownToHtml/MarkdownParagraph.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Collections.Generic;
 
 namespace MarkdownToHtml
@@ -27,6 +28,105 @@ namespace MarkdownToHtml
             }
             html += $"</{tag}>";
             return html;
+        }
+
+        // Parse a plain paragraph
+        private static ParseResult ParseFrom(
+            ArraySegment<string> lines
+        ) {
+            ParseResult result = new ParseResult();
+            LinkedList<IHtmlable> innerContent = new LinkedList<IHtmlable>();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string line = lines[i];
+                if (endsWithAtLeastTwoSpaces(line))
+                {
+                    string shortened = StripTrailingWhitespace(line);
+                    foreach (IHtmlable entry in MarkdownParser.ParseInnerText(shortened))
+                    {
+                        innerContent.AddLast(entry);
+                    }
+                    innerContent.AddLast(
+                        new MarkdownLinebreak()
+                    );
+                } else {
+                    foreach (IHtmlable entry in MarkdownParser.ParseInnerText(line))
+                    {
+                        innerContent.AddLast(entry);
+                    }
+                    /*
+                     * If this is not the last line,
+                     * it doesn't end in a manual linebreak
+                     * and the user hasn't added a space themselves
+                     * we need to add a space at the end
+                     */
+                    if (
+                        (i != (lines.Count - 1))
+                        && (line.Length > 0)
+                        && (line[^1] != ' ')
+                    ) {
+                        innerContent.AddLast(
+                            new MarkdownText(" ")
+                        );
+                    }
+                }
+            }
+            MarkdownParagraph paragraph = new MarkdownParagraph(
+                LinkedListToArray(innerContent)
+            );
+            result.Success = true;
+            result.AddContent(
+                paragraph
+            );
+            return result;
+        }
+
+        private static bool endsWithAtLeastTwoSpaces (
+            string line
+        ) {
+            if (line.Length > 1)
+            {
+                return line.Substring(
+                    line.Length - 2,
+                    2
+                ) == "  ";
+            } else
+            {
+                return false;
+            }
+        }
+
+        private static string StripTrailingWhitespace(
+            string line
+        ) {
+            return StripTrailingCharacter(
+                line,
+                ' '
+            );
+        }
+
+        private static string StripTrailingCharacter(
+            string line,
+            char character
+        ) {
+            while (
+                (line.Length > 0)
+                && (line[^1] == character)
+            ) {
+                line = line.Substring(
+                    0,
+                    line.Length - 1 
+                );
+            }
+            return line;
+        }
+
+        private static T[] LinkedListToArray<T>(
+            LinkedList<T> linkedList
+        ) {
+            T[] array = new T[linkedList.Count];
+            linkedList.CopyTo(array, 0);
+            return array;
         }
 
     }
