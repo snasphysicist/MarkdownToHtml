37f902cf440532da8288f73d5ee618d387dacdd8
snasphysicist <ih8coldplay@hotmail.com>
2020-05-13 21:40:46 +0800

    Single line heading parsing in MarkdownHeading class

--- MarkdownToHtml/MarkdownHeading.cs
+++ MarkdownToHtml/MarkdownHeading.cs
@@ -1,8 +1,15 @@
 
+using System;
+using System.Text.RegularExpressions;
+
 namespace MarkdownToHtml
 {
     public class MarkdownHeading : IHtmlable
     {
+
+        private static Regex regexSingleLineHeading = new Regex(
+            @"^#{1,6}(.+)#*"
+        );
 
         IHtmlable[] content;
 
@@ -58,6 +65,43 @@ namespace MarkdownToHtml
             }
             html += $"</{tag}>";
             return html;
+        }
+
+        public static bool CanParseFrom(
+            ArraySegment<string> lines
+        ) {
+            return regexSingleLineHeading.Match(lines[0]).Success;
+        }
+
+        public static ParseResult ParseFrom(
+            ArraySegment<string> lines
+        ) {
+            ParseResult result = new ParseResult();
+            if (!CanParseFrom(lines))
+            {
+                return result;
+            }
+            // Calculate heading level, (maximum 6)
+            int level = 0;
+            while (
+                (level < 7)
+                 && (lines[0][level] == '#')
+            )
+            {
+                level++;
+            }
+            Match contentMatch = regexSingleLineHeading.Match(lines[0]);
+            string content = contentMatch.Groups[1].Value;
+            lines[0] = "";
+            result.Success = true;
+            result.AddContent(
+                new MarkdownHeading(
+                    level,
+                    MarkdownParser.ParseInnerText(
+                        content
+                    )
+                )
+            );
         }
 
     }
