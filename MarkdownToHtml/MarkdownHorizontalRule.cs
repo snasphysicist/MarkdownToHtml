fc61e5f4ac8dcf84e23ee380afe9615e68115c38
snasphysicist <ih8coldplay@hotmail.com>
2020-05-13 21:08:20 +0800

    Functionality to parse a horizontal rule element in the MarkdownHorizontalRule class

--- MarkdownToHtml/MarkdownHorizontalRule.cs
+++ MarkdownToHtml/MarkdownHorizontalRule.cs
@@ -1,8 +1,17 @@
 
+using System;
+using System.Text.RegularExpressions;
+
 namespace MarkdownToHtml
 {
     public class MarkdownHorizontalRule : IHtmlable
     {
+
+        static Regex regexHorizontalRule = new Regex(
+            @"^-{3,}"
+            + @"^\*{3,}"
+            + @"^\+{3,}"
+        );
 
         string tag = "hr";
 
@@ -13,6 +22,28 @@ namespace MarkdownToHtml
 
         public string ToHtml() {
             return $"<{tag}>";
+        }
+
+        public static bool CanParseFrom(
+            ArraySegment<string> lines
+        ) {
+            return regexHorizontalRule.Match(lines[0]).Success;
+        }
+
+        public static ParseResult ParseFrom(
+            ArraySegment<string> lines
+        ) {
+            ParseResult result = new ParseResult();
+            if (!CanParseFrom(lines))
+            {
+                return result;
+            }
+            lines[0] = "";
+            result.Success = true;
+            result.AddContent(
+                new MarkdownHorizontalRule()
+            );
+            return result;
         }
 
     }
