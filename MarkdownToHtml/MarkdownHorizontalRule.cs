571ca2e3e5033e119b99dd4117e89a5a7b50c445
snasphysicist <ih8coldplay@hotmail.com>
2020-05-13 23:20:11 +0800

    More accurate checking for correct horizontal rule formats - - or * plus whitespace, with minimum - or *

--- MarkdownToHtml/MarkdownHorizontalRule.cs
+++ MarkdownToHtml/MarkdownHorizontalRule.cs
@@ -8,9 +8,8 @@ namespace MarkdownToHtml
     {
 
         static Regex regexHorizontalRule = new Regex(
-            @"^-{3,}"
-            + @"^\*{3,}"
-            + @"^\+{3,}"
+            @"^[\s|\*]{3,}"
+            + @"|^[\s|-]{3,}"
         );
 
         string tag = "hr";
@@ -27,7 +26,14 @@ namespace MarkdownToHtml
         public static bool CanParseFrom(
             ArraySegment<string> lines
         ) {
-            return regexHorizontalRule.Match(lines[0]).Success;
+            // Check if the format is correct (* or - plus whitespace only)
+            bool correctFormat = regexHorizontalRule.Match(lines[0]).Success;
+            // Check there are enough - or * characters (3++)
+            bool enoughNonWhitespace = (
+                (lines[0].Length - lines[0].Replace(" ", "").Length)
+                > 2
+            );
+            return correctFormat && enoughNonWhitespace;
         }
 
         public static ParseResult ParseFrom(
