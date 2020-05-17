976e25a3a8324824fe6ed7284c814d54c0462aba
snasphysicist <ih8coldplay@hotmail.com>
2020-05-17 23:06:10 +0800

    Object to hold relations between reference text and url for links by reference

---
+++ MarkdownToHtml/ReferencedUrl.cs
@@ -0,0 +1,53 @@
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace MarkdownToHtml
+{
+    public class ReferencedUrl
+    {
+
+        private static Regex regexUrlReference = new Regex(
+            @"^\[(.*)[^\\]\]:(.+)"
+        );
+
+        public string Reference
+        { get; private set; }
+
+        public string Url
+        { get; private set; }
+
+        public ReferencedUrl(
+            string reference,
+            string url
+        ) {
+            Reference = reference;
+            Url = url;
+        }
+
+        public static bool CanParseFrom(
+            ArraySegment<string> lines
+        ) {
+            return regexUrlReference.Match(lines[0]).Success;
+        }
+
+        public static ReferencedUrl ParseFrom(
+            ArraySegment<string> lines
+        ) {
+            Match contentMatch = regexUrlReference.Match(
+                lines[0]
+            );
+            string reference = contentMatch.Groups[1].Value;
+            string url = contentMatch.Groups[2].Value;
+            lines[0] = regexUrlReference.Replace(
+                lines[0],
+                ""
+            );
+            return new ReferencedUrl(
+                reference,
+                url
+            );
+        }
+
+    }
+}
