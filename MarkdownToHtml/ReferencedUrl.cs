b668186d6d6cf865f08888e2cc3179ffbf29088d
snasphysicist <ih8coldplay@hotmail.com>
2020-05-19 19:35:36 +0800

    Refactor to work with image links

--- MarkdownToHtml/ReferencedUrl.cs
+++ MarkdownToHtml/ReferencedUrl.cs
@@ -7,9 +7,16 @@ namespace MarkdownToHtml
     public class ReferencedUrl
     {
 
-        private static Regex regexUrlReference = new Regex(
-            @"^\[(.*[^\\])\]:\s*(.+)"
+        private static Regex regexLinkReference = new Regex(
+            @"^\[(.*)[^\\]\]:(.+)"
         );
+
+        private static Regex regexImageReference = new Regex(
+            @"^\[(.*[^\\])\]:\s*(.+?)\s+""(.+?)""\s*$"
+        );
+
+        public ReferencedUrlType Type
+        { get; private set; }
 
         public string Reference
         { get; private set; }
@@ -17,35 +24,68 @@ namespace MarkdownToHtml
         public string Url
         { get; private set; }
 
+        public string Title
+        { get; private set; }
+
         public ReferencedUrl(
             string reference,
-            string url
+            string url,
+            string title,
+            ReferencedUrlType type
         ) {
             Reference = reference;
             Url = url;
+            Title = title;
+            Type = type;
         }
 
         public static bool CanParseFrom(
             ArraySegment<string> lines
         ) {
-            return regexUrlReference.Match(lines[0]).Success;
+            return (
+                regexLinkReference.Match(lines[0]).Success
+                || regexImageReference.Match(lines[0]).Success
+            );
         }
 
         public static ReferencedUrl ParseFrom(
             ArraySegment<string> lines
         ) {
-            Match contentMatch = regexUrlReference.Match(
+            string reference = "";
+            string url = "";
+            string title = "";
+            ReferencedUrlType type = ReferencedUrlType.Link;
+            Match linkMatch = regexLinkReference.Match(
                 lines[0]
             );
-            string reference = contentMatch.Groups[1].Value;
-            string url = contentMatch.Groups[2].Value;
-            lines[0] = regexUrlReference.Replace(
-                lines[0],
-                ""
+            Match imageMatch = regexImageReference.Match(
+                lines[0]
             );
+            if (linkMatch.Success)
+            {
+                reference = linkMatch.Groups[1].Value;
+                url = linkMatch.Groups[2].Value;
+                type = ReferencedUrlType.Link;
+                lines[0] = regexLinkReference.Replace(
+                    lines[0],
+                    ""
+                );
+            } else if (imageMatch.Success)
+            {
+                reference = imageMatch.Groups[1].Value;
+                url = imageMatch.Groups[2].Value;
+                title = imageMatch.Groups[3].Value;
+                type = ReferencedUrlType.Image;
+                lines[0] = regexLinkReference.Replace(
+                    lines[0],
+                    ""
+                );
+            }
             return new ReferencedUrl(
                 reference,
-                url
+                url,
+                title,
+                type
             );
         }
 
