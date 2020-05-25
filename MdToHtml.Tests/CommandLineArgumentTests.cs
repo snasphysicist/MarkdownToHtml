ec0801fd93771fdc5368754187fa881c73a69d07
snasphysicist <ih8coldplay@hotmail.com>
2020-05-25 12:52:50 +0800

    Start point for tests for MdToHtml console application

---
+++ MdToHtml.Tests/CommandLineArgumentTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MdToHtml.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
