2de69f0bc109d62d0f4a28296ecc5c345d4decb4
snasphysicist <ih8coldplay@hotmail.com>
2020-05-25 17:57:08 +0800

    Print help text on invalid arguments, no arguments or h flag

--- MdToHtml/Program.cs
+++ MdToHtml/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿
+using System;
 
 namespace MdToHtml
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string HelpText =
+            "Markdown To HTML Conversion Utility\n\n"
+            + "Usage: MdToHtml -i input_file -o output_file\n\n"
+            + "Flags:\n"
+            + "  -i input_file  -> path to Markdown file to be converted\n"
+            + "  -o output_file -> path to which converted HTML file will be saved\n"
+            + "  -h             -> print help text";
+
+        static void Main(
+            string[] args
+        )
         {
-            Console.WriteLine("Hello World!");
+            ArgumentParser arguments = new ArgumentParser(
+                String.Join(
+                    " ",
+                    args
+                )
+            );
+            if (
+                !arguments.AllArgumentsValid()
+                || arguments.HasValidFlag("h")
+                || arguments.NoValidArguments()
+            )
+            {
+                PrintHelp();
+                System.Environment.Exit(0);
+            }
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine(
+                HelpText
+            );
         }
     }
 }
