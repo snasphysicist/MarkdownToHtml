d0ab990d42f9eef9d45468eb00880367c21bb362
snasphysicist <ih8coldplay@hotmail.com>
2020-05-25 13:08:25 +0800

    Basic tests for class to parse individual command line arguments

--- MdToHtml.Tests/CommandLineArgumentTests.cs
+++ MdToHtml.Tests/CommandLineArgumentTests.cs
@@ -1,13 +1,166 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MdToHtml.Tests
+namespace MdToHtml
 {
     [TestClass]
-    public class UnitTest1
+    public class CommandLineArgumentTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow("")]
+        public void ShouldNotParseEmptyStringAsArgumentFail(
+            string argument
+        )
         {
+            CommandLineArgument parsed = new CommandLineArgument(
+                argument
+            );
+            Assert.IsFalse(
+                parsed.Valid
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("-f", "f")]
+        public void ShouldParseDashCharacterAsFlagNoValueSuccess(
+            string argument,
+            string flag
+        )
+        {
+            CommandLineArgument parsed = new CommandLineArgument(
+                argument
+            );
+            Assert.IsTrue(
+                parsed.Valid
+            );
+            Assert.AreEqual(
+                flag,
+                parsed.Flag
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("/f", "f")]
+        public void ShouldParseSlashCharacterAsFlagNoValueSuccess(
+            string argument,
+            string flag
+        )
+        {
+            CommandLineArgument parsed = new CommandLineArgument(
+                argument
+            );
+            Assert.IsTrue(
+                parsed.Valid
+            );
+            Assert.AreEqual(
+                flag,
+                parsed.Flag
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("-F", "f")]
+        public void ShouldParseDashUpperCharacterAsLowerFlagNoValueSuccess(
+            string argument,
+            string flag
+        )
+        {
+            CommandLineArgument parsed = new CommandLineArgument(
+                argument
+            );
+            Assert.IsTrue(
+                parsed.Valid
+            );
+            Assert.AreEqual(
+                flag,
+                parsed.Flag
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("-FR")]
+        public void ShouldNotParseDashTwoCharactersAsArgumentFail(
+            string argument
+        )
+        {
+            CommandLineArgument parsed = new CommandLineArgument(
+                argument
+            );
+            Assert.IsFalse(
+                parsed.Valid
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("-f test", "f", "test")]
+        [DataRow("-f           test", "f", "test")]
+        public void ShouldParseDashCharacterValueAsArgumentSuccess(
+            string argument,
+            string flag,
+            string value
+        )
+        {
+            CommandLineArgument parsed = new CommandLineArgument(
+                argument
+            );
+            Assert.IsTrue(
+                parsed.Valid
+            );
+            Assert.AreEqual(
+                flag,
+                parsed.Flag
+            );
+            Assert.AreEqual(
+                value,
+                parsed.Value
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("-f test-test", "f", "test-test")]
+        public void ShouldPreserveDashInValueSuccess(
+            string argument,
+            string flag,
+            string value
+        )
+        {
+            CommandLineArgument parsed = new CommandLineArgument(
+                argument
+            );
+            Assert.IsTrue(
+                parsed.Valid
+            );
+            Assert.AreEqual(
+                flag,
+                parsed.Flag
+            );
+            Assert.AreEqual(
+                value,
+                parsed.Value
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("/f test/test", "f", "test-test")]
+        public void ShouldPreserveSlashInValueSuccess(
+            string argument,
+            string flag,
+            string value
+        )
+        {
+            CommandLineArgument parsed = new CommandLineArgument(
+                argument
+            );
+            Assert.IsTrue(
+                parsed.Valid
+            );
+            Assert.AreEqual(
+                flag,
+                parsed.Flag
+            );
+            Assert.AreEqual(
+                value,
+                parsed.Value
+            );
         }
     }
 }
