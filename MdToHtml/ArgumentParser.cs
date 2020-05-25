ae1cdc861d624beda6c3740770ab91961d4ac594
snasphysicist <ih8coldplay@hotmail.com>
2020-05-25 14:06:06 +0800

    Constructor for ArgumentParser, that parses provided string of arguments

---
+++ MdToHtml/ArgumentParser.cs
@@ -0,0 +1,43 @@
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MdToHtml
+{
+    public class ArgumentParser
+    {
+        private static Regex regexArgumentFlag = new Regex(
+            @"\s[-|/][A-Z|a-z][\s+|^]"
+        );
+
+        private List<CommandLineArgument> providedArguments;
+
+        public ArgumentParser(
+            string arguments
+        ) {
+            // Find all flags in the string, and get index where each occurs
+            MatchCollection flagsMatch = regexArgumentFlag.Matches(
+                arguments
+            );
+            int[] matchLocations = new int[flagsMatch.Count];
+            for (int i = 0; i < flagsMatch.Count; i++)
+            {
+                matchLocations[i] = flagsMatch.Item[i].Index;
+            }
+            // Parse each section associated with a flag
+            providedArguments = new LinkedList<CommandLineArgument>();
+            for (int i = 1; i < flagsMatch.Count; i++)
+            {
+                providedArguments.Add(
+                    new CommandLineArgument(
+                        argument.Substring(
+                            matchLocations[i - 1],
+                            matchLocations[i]
+                        )
+                    )
+                );
+            }
+        }
+
+    }
+}
