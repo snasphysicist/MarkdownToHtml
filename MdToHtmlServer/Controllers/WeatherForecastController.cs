d3c292990391d0376555ee79891c4c9c012e7a52
snasphysicist <ih8coldplay@hotmail.com>
2020-05-27 21:19:13 +0800

    Clear out template code, ready for application code

--- MdToHtmlServer/Controllers/WeatherForecastController.cs
+++ MdToHtmlServer/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,32 +9,13 @@ using Microsoft.Extensions.Logging;
 namespace MdToHtmlServer.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [Route("mdtohtml/convert")]
+    public class ConvertController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Post()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
     }
 }
