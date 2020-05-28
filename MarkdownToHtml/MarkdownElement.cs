44d11e5210cf62e928aae9ddd4e2e9f0b38400dc
snasphysicist <ih8coldplay@hotmail.com>
2020-05-28 21:39:45 +0800

    MarkdownElement abstract class with most common variables in Markdown element classes

--- MarkdownToHtml/MarkdownElement.cs
+++ MarkdownToHtml/MarkdownElement.cs
@@ -3,23 +3,13 @@ using System.Collections.Generic;
 
 namespace MarkdownToHtml
 {
-    public class MarkdownElement
+    public abstract class MarkdownElement
     {
-
         MarkdownElementType Type
         { get; set; }
 
-        LinkedList<IHtmlable> content;
+        IHtmlable[] content;
 
-        Dictionary<string, string> attributes;
-
-        MarkdownElement(
-            MarkdownElementType type
-        ) {
-            Type = type;
-            content = new LinkedList<IHtmlable>();
-            attributes = new Dictionary<string, string>();
-        }
-
+        private string tag;
     }
 }
