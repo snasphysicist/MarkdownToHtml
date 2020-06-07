b5e4de7bdd78c8bd6855680db12fb9f93a540c3a
snasphysicist <ih8coldplay@hotmail.com>
2020-06-07 16:16:59 +0100

    Update parent class name to match new base class names

--- MarkdownToHtml/MarkdownEmphasis.cs
+++ MarkdownToHtml/MarkdownEmphasis.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 
 namespace MarkdownToHtml
 {
-    public class MarkdownEmphasis : MarkdownElement, IHtmlable
+    public class MarkdownEmphasis : MarkdownElementWithContent, IHtmlable
     {
 
         private static Regex regexParseable = new Regex(
