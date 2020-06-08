c325119f01446a967e34185bbdfb5f3594041ff3
snasphysicist <ih8coldplay@hotmail.com>
2020-06-08 19:46:22 +0100

    Move code to find the end of a markdown section to shared class

--- MarkdownToHtml/Utils.cs
+++ MarkdownToHtml/Utils.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Collections.Generic;
 
 namespace MarkdownToHtml
@@ -11,6 +12,55 @@ namespace MarkdownToHtml
             T[] array = new T[linkedList.Count];
             linkedList.CopyTo(array, 0);
             return array;
+        }
+
+        /*
+         * Used to find the line on which a
+         * contiguous markdown element section
+         * ends, based on the string used at the
+         * start of the line to indicate the section
+         */
+        public static int FindEndOfSection(
+            ArraySegment<String> lines,
+            string sectionIndicator
+        )
+        {
+            int index = 1;
+            bool previousLineWasWhitespace = false;
+            /* 
+             * Condition
+             * Don't allow index to exceed the number of elements to avoid Exceptions
+             * We want to break the loop when there is a whitespace line
+             * (previousLineWasWhitespace)
+             * followed by a non-whitespace line (!ContainsOnlyWhitespace(lines[index])) 
+             * which is not a line of given section type !lines[index].StartsWith
+             */
+            while (
+                index < lines.Count
+                && !(
+                    previousLineWasWhitespace
+                    && !ContainsOnlyWhitespace(lines[index])
+                    && !lines[index].StartsWith(sectionIndicator)
+                )
+            ) {
+                if (ContainsOnlyWhitespace(lines[index]))
+                {
+                    previousLineWasWhitespace = true;
+                } else {
+                    previousLineWasWhitespace = false;
+                }
+                index++;
+            }
+            return index;
+        }
+
+        private static bool ContainsOnlyWhitespace(
+            string line
+        ) {
+            return line.Replace(
+                " ",
+                ""
+            ).Length == 0;
         }
     }
 }
