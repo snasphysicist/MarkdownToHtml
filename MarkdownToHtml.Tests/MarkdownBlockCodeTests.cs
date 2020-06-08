3a53c7b59b0e82ef25a7e42987e3cf82de3f4a78
snasphysicist <ih8coldplay@hotmail.com>
2020-06-08 19:57:57 +0100

    Test expected behaviour for indented code blocks, all failing

--- MarkdownToHtml.Tests/MarkdownBlockCodeTests.cs
+++ MarkdownToHtml.Tests/MarkdownBlockCodeTests.cs
@@ -49,6 +49,104 @@ namespace MarkdownToHtml
             );
         }
 
+        [DataTestMethod]
+        [Timeout(500)]
+        [DataRow("    test1", "<pre><code>test1</code></pre>")]
+        public void ParseProperlyIndentedCodeBlockAsPreCode(
+            string markdown,
+            string targetHtml
+        ) {
+            MarkdownParser parser = new MarkdownParser(
+                markdown.Split('\n')
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            string html = parser.ToHtml();
+            Assert.AreEqual(
+                targetHtml,
+                html
+            );
+        }
 
+        [DataTestMethod]
+        [Timeout(500)]
+        [DataRow("     test1", "<pre><code> test1</code></pre>")]
+        public void PreserveSpacesInIndentedCodeBlockAfterFirstFour(
+            string markdown,
+            string targetHtml
+        ) {
+            MarkdownParser parser = new MarkdownParser(
+                markdown.Split('\n')
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            string html = parser.ToHtml();
+            Assert.AreEqual(
+                targetHtml,
+                html
+            );
+        }
+
+        [DataTestMethod]
+        [Timeout(500)]
+        [DataRow("    test1\n    test2", "<pre><code>test1\ntest2</code></pre>")]
+        public void GroupAdjacentIndentedLinesIntoSamePreCodeBlock(
+            string markdown,
+            string targetHtml
+        ) {
+            MarkdownParser parser = new MarkdownParser(
+                markdown.Split('\n')
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            string html = parser.ToHtml();
+            Assert.AreEqual(
+                targetHtml,
+                html
+            );
+        }
+
+        [DataTestMethod]
+        [Timeout(500)]
+        [DataRow("    test1\n\n    test2", "<pre><code>test1\n\ntest2</code></pre>")]
+        public void PreserveEmptyLinesBetweenIndentedLinesInPreCodeBlock(
+            string markdown,
+            string targetHtml
+        ) {
+            MarkdownParser parser = new MarkdownParser(
+                markdown.Split('\n')
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            string html = parser.ToHtml();
+            Assert.AreEqual(
+                targetHtml,
+                html
+            );
+        }
+
+        [DataTestMethod]
+        [Timeout(500)]
+        [DataRow("   test1", "<p>test1</p>")]
+        public void ParseThreeSpaceIndentedLineAsParagraph(
+            string markdown,
+            string targetHtml
+        ) {
+            MarkdownParser parser = new MarkdownParser(
+                markdown.Split('\n')
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            string html = parser.ToHtml();
+            Assert.AreEqual(
+                targetHtml,
+                html
+            );
+        }
     }
 }
