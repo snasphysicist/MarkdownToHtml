521497a3798bc94dc877fd123f7655dc570d8c7b
snasphysicist <ih8coldplay@hotmail.com>
2020-06-12 10:10:54 +0100

    Update type to tag converter for new element type class name

--- MarkdownToHtml/TypeToTag.cs
+++ MarkdownToHtml/TypeToTag.cs
@@ -5,37 +5,37 @@ namespace MarkdownToHtml
 {
     public static class TypeToTag
     {
-        private static Dictionary<MarkdownElementType, string> tags
-            = new Dictionary<MarkdownElementType, string>()
+        private static Dictionary<ElementType, string> tags
+            = new Dictionary<ElementType, string>()
         {
-            {MarkdownElementType.Heading1, "h1"},
-            {MarkdownElementType.Heading2, "h2"},
-            {MarkdownElementType.Heading3, "h3"},
-            {MarkdownElementType.Heading4, "h4"},
-            {MarkdownElementType.Heading5, "h5"},
-            {MarkdownElementType.Heading6, "h6"},
-            {MarkdownElementType.Heading1Underlined, "h1"},
-            {MarkdownElementType.Heading2Underlined, "h2"},
-            {MarkdownElementType.Emphasis, "em"},
-            {MarkdownElementType.Strong, "strong"},
-            {MarkdownElementType.Strikethrough, "s"},
-            {MarkdownElementType.OrderedList, "ol"},
-            {MarkdownElementType.UnorderedList, "ul"},
-            {MarkdownElementType.ListItem, "li"},
-            {MarkdownElementType.Link, "a"},
-            {MarkdownElementType.Image, "img"},
-            {MarkdownElementType.CodeInline, "code"},
-            {MarkdownElementType.CodeBlock, "code"},
-            {MarkdownElementType.Quote, "blockquote"},
-            {MarkdownElementType.HorizontalRule, "hr"},
-            {MarkdownElementType.Paragraph, "p"},
-            {MarkdownElementType.Text, ""},
-            {MarkdownElementType.Linebreak, "br"},
-            {MarkdownElementType.Preformatted, "pre"}
+            {ElementType.Heading1, "h1"},
+            {ElementType.Heading2, "h2"},
+            {ElementType.Heading3, "h3"},
+            {ElementType.Heading4, "h4"},
+            {ElementType.Heading5, "h5"},
+            {ElementType.Heading6, "h6"},
+            {ElementType.Heading1Underlined, "h1"},
+            {ElementType.Heading2Underlined, "h2"},
+            {ElementType.Emphasis, "em"},
+            {ElementType.Strong, "strong"},
+            {ElementType.Strikethrough, "s"},
+            {ElementType.OrderedList, "ol"},
+            {ElementType.UnorderedList, "ul"},
+            {ElementType.ListItem, "li"},
+            {ElementType.Link, "a"},
+            {ElementType.Image, "img"},
+            {ElementType.CodeInline, "code"},
+            {ElementType.CodeBlock, "code"},
+            {ElementType.Quote, "blockquote"},
+            {ElementType.HorizontalRule, "hr"},
+            {ElementType.Paragraph, "p"},
+            {ElementType.Text, ""},
+            {ElementType.Linebreak, "br"},
+            {ElementType.Preformatted, "pre"}
         };
 
         public static string Tag(
-            this MarkdownElementType type
+            this ElementType type
         ) {
             string tag = tags[
                 type
