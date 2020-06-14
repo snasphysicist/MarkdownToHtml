46a87dde0acabd72d8dd391236868eb613e14690
snasphysicist <ih8coldplay@hotmail.com>
2020-06-14 16:56:34 +0100

    Methods to allow a list to detect whether the list items should include paragraphs

--- MarkdownToHtml/MarkdownParsers/List.cs
+++ MarkdownToHtml/MarkdownParsers/List.cs
@@ -6,7 +6,12 @@ namespace MarkdownToHtml
 {
     public class List : IMarkdownParser
     {
-        private static IMarkdownParser listItemParser = new ListItem();
+        private static IMarkdownParser listItemRawParser = new ListItem();
+        private static IMarkdownParser listItemParagraphParser = new ListItemParagraph();
+
+        private static Regex regexOrderedListLine = new Regex(
+            @"^[\s]{0,3}\d+\.(\s+?.*)"
+        );
 
         private static Regex regexUnorderedListLine = new Regex(
             @"^[\s]{0,3}[\*|\+|-].*"
@@ -14,10 +19,8 @@ namespace MarkdownToHtml
         public bool CanParseFrom(
             ParseInput input
         ) {
-            return (
-                listItemParser.CanParseFrom(
-                    input
-                )
+            return IsListItemLine(
+                input[0].Text
             );
         }
 
@@ -25,7 +28,10 @@ namespace MarkdownToHtml
             ParseInput input
         ) {
             ParseResult result = new ParseResult();
-            ElementType listType = DeduceListType(
+            ElementType listType = DetermineListType(
+                input
+            );
+            IMarkdownParser listItemParser = DetermineListItemParserType(
                 input
             );
             ParseResult parsedListItem;
@@ -64,7 +70,16 @@ namespace MarkdownToHtml
             return result;
         }
 
-        private ElementType DeduceListType(
+        private bool IsListItemLine(
+            string line
+        ) {
+            return (
+                regexOrderedListLine.Match(line).Success
+                || regexUnorderedListLine.Match(line).Success
+            );
+        }
+
+        private ElementType DetermineListType(
             ParseInput input
         ) {
             if (
@@ -76,6 +91,91 @@ namespace MarkdownToHtml
             } else {
                 return ElementType.OrderedList;
             }
+        }
+
+        private IMarkdownParser DetermineListItemParserType(
+            ParseInput input
+        ) {
+            if (
+                ListItemsWrappedInParagraphs(
+                    input
+                )
+            ) {
+                return listItemParagraphParser;
+            } else {
+                return listItemRawParser;
+            }
+        }
+
+        private bool ListItemsWrappedInParagraphs(
+            ParseInput input
+        ) {
+            // Note: includes trailing whitespace lines
+            int endOfListSection = FindEndOfListSection(
+                input
+            );
+            // Step back to ignore trailing whitespace lines
+            while (
+                input[endOfListSection].ContainsOnlyWhitespace()
+            ) {
+                endOfListSection--;
+            }
+            return ContainsWhitespaceLine(
+                input,
+                endOfListSection
+            );
+        }
+
+        private bool ContainsWhitespaceLine(
+            ParseInput input,
+            int checkUpTo
+        ) {
+            for (int i = 0; i < checkUpTo; i++)
+            {
+                if (input[i].ContainsOnlyWhitespace())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private int FindEndOfListSection(
+            ParseInput input
+        ) {
+            int currentIndex = 1;
+            while (
+                (currentIndex < input.Count)
+                && (
+                    IsListItemLine(
+                        input[currentIndex].Text
+                    )
+                )
+            ) {
+                int endOfThisListItem = FindEndOfListItem(
+                    input,
+                    currentIndex
+                );
+                currentIndex += endOfThisListItem;
+            }
+            return currentIndex;
+        }
+
+        private int FindEndOfListItem(
+            ParseInput input,
+            int startIndex
+        ) {
+            int currentLine = 1;
+            while (
+                (currentLine < input.Count)
+                && !input[currentLine].ContainsOnlyWhitespace()
+                && !IsListItemLine(
+                    input[currentLine].Text
+                ) 
+            ) {
+                currentLine++;
+            }
+            return currentLine;
         }
     }
 }
