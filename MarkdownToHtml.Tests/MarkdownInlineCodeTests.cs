9b9731ad2a4afade38abeaa6b7d86c336bb12d39
snasphysicist <ih8coldplay@hotmail.com>
2020-06-14 13:14:22 +0100

    Inline code tests passing

--- MarkdownToHtml.Tests/MarkdownInlineCodeTests.cs
+++ MarkdownToHtml.Tests/MarkdownInlineCodeTests.cs
@@ -9,7 +9,6 @@ namespace MarkdownToHtml
 
         [DataTestMethod]
         [Timeout(500)]
-        [Ignore]
         [DataRow("`test1`", "<p><code>test1</code></p>")]
         [DataRow("test1`test2`test3", "<p>test1<code>test2</code>test3</p>")]
         public void ShouldParseCorrectlyFormattedInlineCodeSuccess(
@@ -34,7 +33,6 @@ namespace MarkdownToHtml
 
         [DataTestMethod]
         [Timeout(500)]
-        [Ignore]
         [DataRow("`te\\`st1`", "<p><code>te`st1</code></p>")]
         public void ShouldParseCorrectlyEscapedStrikethroughCharactersSuccess(
             string markdown,
@@ -58,7 +56,6 @@ namespace MarkdownToHtml
 
         [DataTestMethod]
         [Timeout(500)]
-        [Ignore]
         [DataRow("`test1", "<p>`test1</p>")]
         public void ShouldParseIncorrectlyDelimitedInlineCodeAsParagraphSuccess(
             string markdown,
