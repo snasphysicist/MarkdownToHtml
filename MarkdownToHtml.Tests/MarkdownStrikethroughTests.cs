077275c186b8e4dd5585a27725fbf884ca11cb15
snasphysicist <ih8coldplay@hotmail.com>
2020-06-14 12:58:43 +0100

    Remaining strikethrough tests passing

--- MarkdownToHtml.Tests/MarkdownStrikethroughTests.cs
+++ MarkdownToHtml.Tests/MarkdownStrikethroughTests.cs
@@ -33,7 +33,6 @@ namespace MarkdownToHtml
 
         [DataTestMethod]
         [Timeout(500)]
-        [Ignore]
         [DataRow("~~te\\~st1~~", "<p><s>te~st1</s></p>")]
         public void ShouldParseCorrectlyEscapedStrikethroughCharactersSuccess(
             string markdown,
@@ -57,7 +56,6 @@ namespace MarkdownToHtml
 
         [DataTestMethod]
         [Timeout(500)]
-        [Ignore]
         [DataRow("~~test1", "<p>~~test1</p>")]
         public void ShouldParseIncorrectlyDelimitedStrikethroughAsParagraphSuccess(
             string markdown,
