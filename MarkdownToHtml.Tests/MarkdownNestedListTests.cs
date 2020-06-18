7fadf42758cfccf0bf2c2275f3e14b8e1739633b
snasphysicist <ih8coldplay@hotmail.com>
2020-06-18 11:18:44 +0100

    Basic tests for ordered lists nested in unordered lists

--- MarkdownToHtml.Tests/MarkdownNestedListTests.cs
+++ MarkdownToHtml.Tests/MarkdownNestedListTests.cs
@@ -32,11 +32,35 @@ namespace MarkdownToHtml
 
         [DataTestMethod]
         [Timeout(500)]
-        [DataRow("1. test1\n    * test2", "<ol><li>test1<ol><li>test2</li></ol></li></ol>")]
-        [DataRow("1. test1\n     + test2", "<ol><li>test1<ol><li>test2</li></ol></li></ol>")]
-        [DataRow("1. test1\n      - test2", "<ol><li>test1<ol><li>test2</li></ol></li></ol>")]
-        [DataRow("1. test1\n       * test2", "<ol><li>test1<ol><li>test2</li></ol></li></ol>")]
+        [DataRow("1. test1\n    * test2", "<ol><li>test1<ul><li>test2</li></ul></li></ol>")]
+        [DataRow("1. test1\n     + test2", "<ol><li>test1<ul><li>test2</li></ul></li></ol>")]
+        [DataRow("1. test1\n      - test2", "<ol><li>test1<ul><li>test2</li></ul></li></ol>")]
+        [DataRow("1. test1\n       * test2", "<ol><li>test1<ul><li>test2</li></ul></li></ol>")]
         public void UnorderedListItemOneIdentationLevelInFromOrderedListItemCreatesNestedOrderedList(
+            string markdown,
+            string targetHtml
+        ) {
+            MarkdownParser parser = new MarkdownParser(
+                markdown.Split(
+                    "\n"
+                )
+            );
+            Assert.IsTrue(
+                parser.Success
+            );
+            Assert.AreEqual(
+                targetHtml,
+                parser.ToHtml()
+            );
+        }
+
+        [DataTestMethod]
+        [Timeout(500)]
+        [DataRow("* test1\n    1. test2", "<ul><li>test1<ol><li>test2</li></ol></li></ul>")]
+        [DataRow("+ test1\n     1. test2", "<ul><li>test1<ol><li>test2</li></ol></li></ul>")]
+        [DataRow("- test1\n      1. test2", "<ul><li>test1<ol><li>test2</li></ol></li></ul>")]
+        [DataRow("* test1\n       1. test2", "<ul><li>test1<ol><li>test2</li></ol></li></ul>")]
+        public void OrderedListItemOneIdentationLevelInFromUnorderedListItemCreatesNestedOrderedList(
             string markdown,
             string targetHtml
         ) {
