f2103cf83db69b448666b4448134d76119905694
snasphysicist <ih8coldplay@hotmail.com>
2020-06-27 20:41:16 +0100

    Comment out intermediate list item classes temporarily (probably no longer needed)

--- MarkdownToHtml/MarkdownParsers/ListItemParagraph.cs
+++ MarkdownToHtml/MarkdownParsers/ListItemParagraph.cs
@@ -1,57 +1,57 @@
 
-namespace MarkdownToHtml
-{
-    public class ListItemParagraph : IMarkdownParser
-    {
-        private IMarkdownParser innerParser;
+// namespace MarkdownToHtml
+// {
+//     public class ListItemParagraph : IMarkdownParser
+//     {
+//         private IMarkdownParser innerParser;
 
-        private int indentationLevel;
+//         private int indentationLevel;
 
-        public ListItemParagraph(
-            int indentationLevel
-        ) {
-            this.indentationLevel = indentationLevel;
-            innerParser = new ListItemInner(
-                indentationLevel
-            );
-        }
+//         public ListItemParagraph(
+//             int indentationLevel
+//         ) {
+//             this.indentationLevel = indentationLevel;
+//             innerParser = new ListItemInner(
+//                 indentationLevel
+//             );
+//         }
 
-        public bool CanParseFrom(
-            ParseInput input
-        ) {
-            return innerParser.CanParseFrom(
-                input
-            );
-        }
+//         public bool CanParseFrom(
+//             ParseInput input
+//         ) {
+//             return innerParser.CanParseFrom(
+//                 input
+//             );
+//         }
 
-        public ParseResult ParseFrom(
-            ParseInput input
-        ) {
-            ParseResult result = new ParseResult();
-            if (!CanParseFrom(input))
-            {
-                return result;
-            }
-            ParseResult innerContent = innerParser.ParseFrom(
-                input
-            );
-            if (!innerContent.Success)
-            {
-                return result;
-            }
-            Element paragraph = new ElementFactory().New(
-                ElementType.Paragraph,
-                innerContent.GetContent()
-            );
-            Element listItem = new ElementFactory().New(
-                ElementType.ListItem,
-                paragraph
-            );
-            result.Success = true;
-            result.AddContent(
-                listItem
-            );
-            return result;
-        }
-    }
-}
+//         public ParseResult ParseFrom(
+//             ParseInput input
+//         ) {
+//             ParseResult result = new ParseResult();
+//             if (!CanParseFrom(input))
+//             {
+//                 return result;
+//             }
+//             ParseResult innerContent = innerParser.ParseFrom(
+//                 input
+//             );
+//             if (!innerContent.Success)
+//             {
+//                 return result;
+//             }
+//             Element paragraph = new ElementFactory().New(
+//                 ElementType.Paragraph,
+//                 innerContent.GetContent()
+//             );
+//             Element listItem = new ElementFactory().New(
+//                 ElementType.ListItem,
+//                 paragraph
+//             );
+//             result.Success = true;
+//             result.AddContent(
+//                 listItem
+//             );
+//             return result;
+//         }
+//     }
+// }
--- MarkdownToHtml/MarkdownParsers/ListItemRaw.cs
+++ MarkdownToHtml/MarkdownParsers/ListItemRaw.cs
@@ -1,53 +1,53 @@
 
-namespace MarkdownToHtml
-{
-    public class ListItemRaw : IMarkdownParser
-    {
-        private IMarkdownParser innerParser;
+// namespace MarkdownToHtml
+// {
+//     public class ListItemRaw : IMarkdownParser
+//     {
+//         private IMarkdownParser innerParser;
 
-        private int indentationLevel;
+//         private int indentationLevel;
 
-        public ListItemRaw(
-            int indentationLevel
-        ) {
-            this.indentationLevel = indentationLevel;
-            innerParser = new ListItemInner(
-                indentationLevel
-            );
-        }
+//         public ListItemRaw(
+//             int indentationLevel
+//         ) {
+//             this.indentationLevel = indentationLevel;
+//             innerParser = new ListItemInner(
+//                 indentationLevel
+//             );
+//         }
 
-        public bool CanParseFrom(
-            ParseInput input
-        ) {
-            return innerParser.CanParseFrom(
-                input
-            );
-        }
+//         public bool CanParseFrom(
+//             ParseInput input
+//         ) {
+//             return innerParser.CanParseFrom(
+//                 input
+//             );
+//         }
 
-        public ParseResult ParseFrom(
-            ParseInput input
-        ) {
-            ParseResult result = new ParseResult();
-            if (!CanParseFrom(input))
-            {
-                return result;
-            }
-            ParseResult innerContent = innerParser.ParseFrom(
-                input
-            );
-            if (!innerContent.Success)
-            {
-                return result;
-            }
-            Element listItem = new ElementFactory().New(
-                ElementType.ListItem,
-                innerContent.GetContent()
-            );
-            result.Success = true;
-            result.AddContent(
-                listItem
-            );
-            return result;
-        }
-    }
-}
+//         public ParseResult ParseFrom(
+//             ParseInput input
+//         ) {
+//             ParseResult result = new ParseResult();
+//             if (!CanParseFrom(input))
+//             {
+//                 return result;
+//             }
+//             ParseResult innerContent = innerParser.ParseFrom(
+//                 input
+//             );
+//             if (!innerContent.Success)
+//             {
+//                 return result;
+//             }
+//             Element listItem = new ElementFactory().New(
+//                 ElementType.ListItem,
+//                 innerContent.GetContent()
+//             );
+//             result.Success = true;
+//             result.AddContent(
+//                 listItem
+//             );
+//             return result;
+//         }
+//     }
+// }
