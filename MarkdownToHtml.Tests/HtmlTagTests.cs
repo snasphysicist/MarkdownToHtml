43cd1edd4b118d080db698b0e5ffece47c452e85
snasphysicist <ih8coldplay@hotmail.com>
2021-01-18 20:01:07 +0000

    / must be next to >

--- MarkdownToHtml.Tests/HtmlTagTests.cs
+++ MarkdownToHtml.Tests/HtmlTagTests.cs
@@ -697,5 +697,85 @@ namespace MarkdownToHtml
             HtmlSnippet detected = detector.Detect();
             Assert.IsFalse(detected.IsTag());
         }
+
+        [TestMethod]
+        [Timeout(500)]
+        public void SelfClosingTagWithWhitespaceBetweenSlashAndCloserIsNotAValidHtmlTag()
+        {
+            HtmlToken[] tokens = new HtmlToken[]
+            {
+                new HtmlToken(
+                    HtmlTokenType.LessThan,
+                    "<"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Text,
+                    "p"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.NonLineBreakingWhitespace,
+                    "  "
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Text,
+                    "a"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Equals,
+                    "="
+                ),
+                new HtmlToken(
+                    HtmlTokenType.DoubleQuote,
+                    "\""
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Text,
+                    "yhtvc754"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.DoubleQuote,
+                    "\""
+                ),
+                new HtmlToken(
+                    HtmlTokenType.NonLineBreakingWhitespace,
+                    "\t\t\t"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Text,
+                    "ctvyh745"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Equals,
+                    "="
+                ),
+                new HtmlToken(
+                    HtmlTokenType.DoubleQuote,
+                    "\""
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Text,
+                    "3498-vnt58y7"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.DoubleQuote,
+                    "\""
+                ),
+                new HtmlToken(
+                    HtmlTokenType.ForwardSlash,
+                    "/"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.LineBreakingWhitespace,
+                    "\n\n\n\n"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.GreaterThan,
+                    ">"
+                )
+            };
+            HtmlTagDetector detector = new HtmlTagDetector(tokens);
+            HtmlSnippet detected = detector.Detect();
+            Assert.IsFalse(detected.IsTag());
+        }
     }
 }
