d4ba316b39a5ef3b37f0d072befd3081454b0ca4
snasphysicist <ih8coldplay@hotmail.com>
2021-01-21 20:33:59 +0000

    Ensure tag name detected correctly for opening tag with attributes

--- MarkdownToHtml.Tests/HtmlTagNameDetectionTests.cs
+++ MarkdownToHtml.Tests/HtmlTagNameDetectionTests.cs
@@ -32,7 +32,80 @@ namespace MarkdownToHtml
                 "p"
             );
         }
+
+        [TestMethod]
+        [Timeout(500)]
+        public void TextDirectlyAfterOpenerIsTheTagNameForAnOpeningTagWithAttributes()
+        {
+            HtmlToken[] tokens = new HtmlToken[]
+            {
+                new HtmlToken(
+                    HtmlTokenType.LessThan,
+                    "<"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Text,
+                    "help"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.NonLineBreakingWhitespace,
+                    "  "
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Text,
+                    "a"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Equals,
+                    "="
+                ),
+                new HtmlToken(
+                    HtmlTokenType.DoubleQuote,
+                    "\""
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Text,
+                    "yhtvc754"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.DoubleQuote,
+                    "\""
+                ),
+                new HtmlToken(
+                    HtmlTokenType.NonLineBreakingWhitespace,
+                    "\t\t\t"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Text,
+                    "ctvyh745"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Equals,
+                    "="
+                ),
+                new HtmlToken(
+                    HtmlTokenType.DoubleQuote,
+                    "\""
+                ),
+                new HtmlToken(
+                    HtmlTokenType.Text,
+                    "3498-vnt58y7"
+                ),
+                new HtmlToken(
+                    HtmlTokenType.DoubleQuote,
+                    "\""
+                ),
+                new HtmlToken(
+                    HtmlTokenType.GreaterThan,
+                    ">"
+                )
+            };
+            HtmlTagDetector detector = new HtmlTagDetector(tokens);
+            HtmlSnippet detected = detector.Detect();
+            Assert.AreEqual(
+                "help",
+                detected.Tag.Name.Name
+            );
+        }
     }
 }
-
-
