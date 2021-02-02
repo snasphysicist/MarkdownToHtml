ebf1586a0848a4a2cd3c5627e0e584d57444d9fd
snasphysicist <ih8coldplay@hotmail.com>
2021-02-02 19:32:27 +0000

    Ensure input with multiple replacements handled properly

--- MarkdownToHtml.Tests/HtmlElementInserterTests.cs
+++ MarkdownToHtml.Tests/HtmlElementInserterTests.cs
@@ -28,7 +28,8 @@ namespace MarkdownToHtml
         public void NoReplacementsAreMadeNothingInTheInputStringMatchesAGuidInTheDictionary()
         {
             string toProcess = "Not A Guid, A Guid " + Guid.NewGuid().ToString();
-            Dictionary<Guid, string> replacements = new Dictionary<Guid, string>{
+            Dictionary<Guid, string> replacements = new Dictionary<Guid, string>
+            {
                 {Guid.NewGuid(), "<p>"}
             };
             HtmlElementInserter inserter = new HtmlElementInserter(
@@ -48,7 +49,8 @@ namespace MarkdownToHtml
             Guid guid = Guid.NewGuid();
             string toProcess = guid.ToString();
             string replaced = "<p>";
-            Dictionary<Guid, string> replacements = new Dictionary<Guid, string>{
+            Dictionary<Guid, string> replacements = new Dictionary<Guid, string>
+            {
                 {guid, replaced},
                 {Guid.NewGuid(), "Something else"}
             };
@@ -71,7 +73,8 @@ namespace MarkdownToHtml
             string suffix = " Test " + Guid.NewGuid().ToString() + " Another Guid...";
             string toProcess = prefix + guid.ToString() + suffix;
             string replaced = "<p>";
-            Dictionary<Guid, string> replacements = new Dictionary<Guid, string>{
+            Dictionary<Guid, string> replacements = new Dictionary<Guid, string>
+            {
                 {guid, replaced},
                 {Guid.NewGuid(), "Something else"}
             };
@@ -81,6 +84,44 @@ namespace MarkdownToHtml
             );
             Assert.AreEqual(
                 prefix + replaced + suffix,
+                inserter.Processed
+            );
+        }
+
+        [TestMethod]
+        [Timeout(500)]
+        public void AllMatchingGuidsInInputStringAreReplacedByReplacements()
+        {
+            Dictionary<Guid, string> replacements = new Dictionary<Guid, string>
+            {
+                {Guid.NewGuid(), "<p>"},
+                {Guid.NewGuid(), "</p>"},
+                {Guid.NewGuid(), "<div>"},
+                {Guid.NewGuid(), "</div>"}
+            };
+            string[] parts = new string[]
+            {
+                " A ",
+                " B ",
+                " " + Guid.NewGuid().ToString() + " ",
+                " D ",
+                " E ",
+            };
+            string toProcess = parts[0];
+            string expected = parts[0];
+            int i = 1;
+            foreach (Guid guid in replacements.Keys)
+            {
+                toProcess = toProcess + guid.ToString() + parts[i];
+                expected = expected + replacements[guid] + parts[i];
+                i++;
+            }
+            HtmlElementInserter inserter = new HtmlElementInserter(
+                replacements,
+                toProcess
+            );
+            Assert.AreEqual(
+                expected,
                 inserter.Processed
             );
         }
